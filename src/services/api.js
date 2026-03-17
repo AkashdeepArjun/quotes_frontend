75e29244b3640a3ed47978be695033dd1a40c4d2
akashdeep <wadhwaarjun007@gmail.com>
2026-03-17 17:45:14 +0530

    changed localhost to live backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({baseURL:"http://localhost:3000/api"});
+const api = axios.create({baseURL:"https://quotes-api-6def.onrender.com/api"});
 
 
 api.interceptors.request.use ( (config) => {
